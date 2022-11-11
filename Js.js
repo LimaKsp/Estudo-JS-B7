2d94717d7bfa049c98e3d323aec8edb546ba52ff
Gabriel de Lima <114449624+LimaKsp@users.noreply.github.com>
2022-11-10 21:04:44 -0300

    Add files via upload

--- Js.js
+++ Js.js
@@ -1,6 +1,5 @@
-var time = 18 ;
+var time = 18;
 var name = "Gabriel";
-
 
 /* Exemplo 1 */
 
@@ -17,22 +16,30 @@ var name = "Gabriel";
 //         alert("boa noite!");
 //     }
 
-
-
 /* Exemplo 2 */
 
-    if (time >= 6 && time <= 12) {
-        console.log("buenos dias")
-    }
-        else if( time >= 12 && time <= 18) {
-        console.log("buenas tarde")
-        }
-            else if (time >= 18 && time <= 23){
-                console.log("buenas notches")
-            }
-
+if (time >= 6 && time <= 12) {
+  console.log("buenos dias");
+} else if (time >= 12 && time <= 18) {
+  console.log("buenas tarde");
+} else if (time >= 18 && time <= 23) {
+  console.log("buenas notches");
+}
 
 /* DOM */
 
-document.getElementById("exemplo")
-.innerHTML = "Msg alterada";
+document.getElementById("exemplo").innerHTML = "Msg alterada";
+
+/* Exemplo Get by Class  e Alteração por índice */
+
+document.getElementsByClassName("alterar")[0].innerHTML =
+  " A mensagem foi alterada por índice";
+
+/* Exemplo Get by Tag Name*/
+
+document.getElementsByTagName("button")[0].innerHTML =
+  " Este botão foi alterado";
+
+/* Exemplo Get by Input */
+
+document.getElementsByName("email")[0].innerHTML = "alterar por nome";
